2390717e69fe3411718cb3e0fc6a7e37119b431f
Raymond Gonzales <146864496+RayTzie@users.noreply.github.com>
2023-11-28 20:30:56 +0800

    Update index.php
    Adding syllabus id,code,author onto index

--- index.php
+++ index.php
@@ -16,6 +16,9 @@
                 <td>ID</td>
                 <td>Subject Code</td>
                 <td>Subject Name</td>
+                <td>Syllabus ID</td>
+                <td>Syllabus Code</td>
+                <td>Syllabus Author</td>
                 <td>Action</td>
             </tr>
             <?php
@@ -24,6 +27,9 @@
                     echo "<td>" . $res["Subject_ID"] . "</td>";
                     echo "<td>" . $res['Subject_Code'] . "</td>";
                     echo "<td>" . $res['Subject_Name'] . "</td>";
+                    echo "<td>" . $res['Syllabus_ID'] . "</td>";
+                    echo "<td>" . $res['syllabus_code'] . "</td>";
+                    echo "<td>" . $res['syllabus_author'] . "</td>";
                     echo "<td><a href=\"forms/editform.php?id=$res[Subject_ID]\">Edit</a> |
                         <a href=\"functions/delete.php?id=$res[Subject_ID]\"
                         onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
